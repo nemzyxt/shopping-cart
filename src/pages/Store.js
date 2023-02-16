9c363be42a4e546955ec21b6018be370007409ce
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-16 15:07:30 +0300

    Update page

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
 
 const Store = () => {
   return (
-    <div>
+    <>
       <h1>Welcome to the Store</h1>
-    </div>
+      <Row xs={1} md={3} className='g-4'>
+        <Col align='center'>
+            <h1>Product</h1>
+        </Col>
+        <Col align='center'>
+            <h1>Product</h1>
+        </Col>
+        <Col align='center'>
+            <h1>Product</h1>
+        </Col>
+      </Row>
+    </>
   )
 }
 
