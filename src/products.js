31154bf4fd4b9285bf65afcd4185fe537e90ad1e
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-16 15:09:08 +0300

    Define products list

---
+++ src/products.js
@@ -0,0 +1,17 @@
+const products = [
+    {
+        id: 1,
+        item: "Coffee",
+        price: 4.99
+    },
+    {
+        id: 2,
+        item: "Coffee",
+        price: 9.99
+    },
+    {
+        id: 3,
+        item: "Coffee",
+        price: 9.99
+    }
+]
