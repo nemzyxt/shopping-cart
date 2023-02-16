f2e98aa1de03bc666529c0d6d68acf9ce6c11ddf
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-16 14:57:39 +0300

    Define our router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <NavBar />
+    <Container>
+      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={ <Store /> } />
+          <Route path="success" element={ <Success /> } />
+          <Route path="cancel" element={ <Cancel /> } />
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
