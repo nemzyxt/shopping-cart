7e566756cff5b19983f61fe6ea592bde4a4fc280
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-16 15:01:27 +0300

    Import pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Container } from 'react-bootstrap';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Store from './pages/Store';
+import Cancel from './pages/Cancel';
+import Success from './pages/Success';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavBar from './components/Navbar';
